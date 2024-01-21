a64944132c41a2f57f18d4d12543e1cf77740361
Diana11d <148574574+Diana11d@users.noreply.github.com>
2024-01-20 21:54:49 -0600

    security group

--- class2repeat/ec2/main.tf
+++ class2repeat/ec2/main.tf
@@ -14,6 +14,19 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_security_group" "class2-ec2" {
+  name        = "class-ec2"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+}
+
+
+
+
+
+
+
+
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
