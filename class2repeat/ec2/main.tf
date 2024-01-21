e29bcf59d4e72214530c0f006453365fb0cf8c40
Diana11d <148574574+Diana11d@users.noreply.github.com>
2024-01-20 21:50:38 -0600

    making changes

--- class2repeat/ec2/main.tf
+++ class2repeat/ec2/main.tf
@@ -18,5 +18,5 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   associate_public_ip_address = false
-  availability_zone = "us-eas-1b"
+  availability_zone = "us-east-1b"
 }
