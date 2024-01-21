60da9508fb47a49bed3e8c9c0d71add1d247ca27
Diana11d <148574574+Diana11d@users.noreply.github.com>
2024-01-20 19:08:03 -0600

    adding new tags

--- class1repeat/main.tf
+++ class1repeat/main.tf
@@ -1,7 +1,13 @@
 resource "aws_iam_user" "devops-diana" {
   name = "devops-diana"
+  tags = {
+    class = "terraform"
+  }
   }
 
 resource "aws_iam_user" "devops-diana2" {
   name = "devops-diana2"
+    tags = {
+    class = "terraform"
+  }
   }
