2ae4c1ae03750d3a0a01a6fa2ba9388ab7199eb0
Diana11d <148574574+Diana11d@users.noreply.github.com>
2024-01-20 22:23:24 -0600

    new output

--- class2repeat/ec2/output.tf
+++ class2repeat/ec2/output.tf
@@ -1,14 +1,14 @@
-output ami  {
+output "ami"  {
     description = "Pulls ami data"
   value       = "aws_instance.web.ami"
 }
  
-output public_ip {
+output "public_ip" {
      description = "Pulls instance public IP"
   value       = "aws_instance.web.public_ip"
 }
  
- output id {
+ output "id" {
      description = "Pulls ami  ID"
   value       = "aws_instance.web.id"
 }
