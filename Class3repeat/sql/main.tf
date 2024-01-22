2daed3b9f9148d799d8fdfc6128cb3c443b0982a
Diana11d <148574574+Diana11d@users.noreply.github.com>
2024-01-21 20:25:13 -0600

    result

--- Class3repeat/sql/main.tf
+++ Class3repeat/sql/main.tf
@@ -3,8 +3,19 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
+
+resource "random_string" "random" {
+  length           = 12
+  special          = false
+  override_special = "/@£$"
+  lower            = true 
+  upper            = false 
+  numeric          = false
+}
+
+
 resource "azurerm_mysql_server" "example" {
-  name                = "example-mysqlserver"
+  name                = "example-${random_string.random.result}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
